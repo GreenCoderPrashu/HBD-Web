7da4c8c5e642d45b4acef3d705d6c3d0cd733089
GreenCoderPrashu <pr829639@gmail.com>
2026-05-25 22:48:28 +0530

    script for wish container.

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,16 @@ function surprise(){
     document.getElementById('home').style.display ='none';
     document.getElementById('wish-container').style.display ='block';
 }
+//
+const typing = document.getElementById('wish');
+const typeBtn= document.getElementById('surprise');
+const passage = "Wish You Many More Happy Returns Of The Day Friend... Stay Happy With All Your Favoriote Things! May This Year Bring You Joy,Succes,And Advanture... And I Always Wish You To Be Happy...";
+let type=""
+let write = false;
+let i=0;
+typeBtn.addEventListener('click',() => {
+    if(! write){
+        write = true;
+        typewriter();
+    }
+});
