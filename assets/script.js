4227ddf1ad1a8ecd497ce8bc4d1dbe23d01c7a52
GreenCoderPrashu <pr829639@gmail.com>
2026-06-08 12:39:25 +0530

    writting script to display image card.

--- assets/script.js
+++ assets/script.js
@@ -31,3 +31,38 @@ function thanks(){
     document.getElementById('wish-container').style.display ='none';
     document.getElementById('page1').style.display ='flex';
 }
+const  TqBtn= document.getElementById('thank-you');
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        card1.style.display = 'block';
+    },1000);
+});
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        card1.style.display = 'none';
+        card2.style.display = 'block';
+    },3000);
+});
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        card2.style.display = 'none';
+        card3.style.display = 'block';
+    },5000);
+});
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        card3.style.display = 'none';
+        card4.style.display = 'block';
+    },7000);
+});
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        card4.style.display = 'none';
+        card5.style.display = 'block';
+    },9000);
+});
+TqBtn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        next.style.display = 'block';
+    },12000);
+});
